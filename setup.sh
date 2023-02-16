834c7ff2f68d930283cca2fb24783f4faf6b8a65
Transistor427 <427departament@gmail.com>
2023-02-16 13:31:53 +0300

    2

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# s
-
+echo "Create service."
 sudo chmod 777 ./rst_nm.sh
 sudo cp ./rst_nm.service /lib/systemd/system/
+
+echo "Enable service."
 sudo systemctl enable rst_nm.service
 
+echo "Create wpa_supplicant.conf"
 sudo cp ./wpa_supplicant.conf /etc/wpa_supplicant.conf
 
+echo "Setup interface."
 sudo chmod 777 /etc/network/interfaces
 sudo echo 'auto wlan0' >> /etc/network/interfaces
 sudo echo 'iface wlan0 inet dhcp' >> /etc/network/interfaces
 sudo echo 'pre-up sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant.conf' >> /etc/network/interfaces
 sudo echo 'post-down killall -q wpa_supplicant' >> /etc/network/interfaces
 sudo chmod 644 /etc/network/interfaces
+
+echo "Done."
